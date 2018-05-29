4b5997e74a019a49cc09b0819c26fd3e87552a8c
kailin.guo <kailin.guo@geely.com>
2018-05-29 16:35:16 +0800

    ffmpeg 视频截取动图

--- src/main/java/video/screenshot/ScreenShotProcess.java
+++ src/main/java/video/screenshot/ScreenShotProcess.java
@@ -26,7 +26,7 @@ public class ScreenShotProcess {
         List<String> commands = new ArrayList<String>();
         commands.add(ffmpeg_path);
         commands.add("-ss");
-        commands.add("\"" + time + "\"");
+        commands.add(String.valueOf(time));
         commands.add("-i");
         commands.add(video_path);
         commands.add("-y");
@@ -40,6 +40,38 @@ public class ScreenShotProcess {
             builder.command(commands);
             builder.start();
             System.out.println(video_path + ",第 " + time + " 秒截取成功...");
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static boolean processGif(String video_path, String ffmpeg_path, String image_name, float time) {
+        File file = new File(video_path);
+        if (!file.exists()) {
+            System.out.println("路径[" + video_path + "]对应的视频文件不存在!");
+            return false;
+        }
+        List<String> commands = new ArrayList<String>();
+        commands.add(ffmpeg_path);
+        commands.add("-ss");
+        commands.add("00:00:20");
+        commands.add("-t");
+        commands.add(String.valueOf(time));
+        commands.add("-i");
+        commands.add(video_path);
+        commands.add("-vf");
+        commands.add("crop=iw:ih*2/3");
+        commands.add("-y");
+        commands.add("-s");
+        commands.add("320x240");
+        commands.add("target/classes/video/" + image_name + ".gif");
+        try {
+            ProcessBuilder builder = new ProcessBuilder();
+            builder.command(commands);
+            builder.start();
+            System.out.println(video_path + "动图生成成功...");
             return true;
         } catch (Exception e) {
             e.printStackTrace();
--- src/test/java/video/screenshot/ScreenShotProcessTest.java
+++ src/test/java/video/screenshot/ScreenShotProcessTest.java
@@ -10,10 +10,20 @@ public class ScreenShotProcessTest {
     public void processImg() throws Exception {
         String video_path = "target/classes/video/Wildlife.wmv",
             ffmpeg_path = "target/classes/tool/ffmpeg.exe",
-        image_name = "shot";
+        image_name = "img";
         float time = 5;
 
         ScreenShotProcess.processImg(video_path, ffmpeg_path, image_name, time);
+    }
+
+    @Test
+    public void processGif() throws Exception {
+        String video_path = "target/classes/video/Wildlife.wmv",
+                ffmpeg_path = "target/classes/tool/ffmpeg.exe",
+                image_name = "gif";
+        float time = 10;
+
+        ScreenShotProcess.processGif(video_path, ffmpeg_path, image_name, time);
     }
 
 }
